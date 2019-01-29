91b6bc2752ea93652d5f02ba09ce8abb47d67250
Cleo Roti <cleo@inreach.tech>
2019-01-29 11:01:47 +0200

    add signalr

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,33 @@
+﻿using HeroesWebApi.HeroesConfig;
+using HeroesWebApi.Services;
+using HeroesWebApi.TimerFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace HeroesWebApi.Controllers
+{
+    public class HeroController: ControllerBase
+    {
+        private readonly HeroesService _heroesService;
+
+        private IHubContext<HeroHub> _hub;
+
+        public HeroController(HeroesService heroesService, IHubContext<HeroHub> hub)
+        {
+            _heroesService = heroesService;
+            _hub = hub;
+   
+        }
+
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferherodata", _heroesService.GetListOfHeroes()));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HeroesWebApi.HeroesConfig;
 using HeroesWebApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,7 +27,18 @@ namespace HeroesWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<HeroesService>();
+            //services.AddScoped<HeroesService>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddSignalR();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
         }
@@ -44,6 +56,13 @@ namespace HeroesWebApi
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<HeroHub>("/heroes");
+            });
+
             app.UseMvc();
         }
     }
